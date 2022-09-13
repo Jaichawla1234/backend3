02cac05d32b99f04558cbd9a34d891ac3cc30bb9
Jaichawla1234 <jai05720803120@bpitindia.com>
2022-09-13 12:43:38 +0530

    commit1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const path=require('path');
 
 const databaseConnect = require('./backend/config/database')
 const authRouter = require('./backend/routes/authRoute')
@@ -16,6 +17,11 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/api/messenger',authRouter);
 app.use('/api/messenger',messengerRoute);
+
+app.use(express.static (path.join(__dirname, "/frontend")));
+
+app.get('', (req, res) => { res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
 
 
 const PORT = process.env.PORT || 5000
